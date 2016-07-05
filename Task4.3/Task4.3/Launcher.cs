8043fcbb0a5d81aa3592fd48b31d8ffc8072dd31
AnastasiyaKulakova <kylalova.n@mail.ru>
2016-07-05 16:36:36 +0300

    add

---
+++ Task4.3/Task4.3/Launcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._3
+{
+    class Launcher
+    {
+        public static int counOfCopies;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public Launcher()
+        {
+            counOfCopies++;
+        }
+
+    }
+}
