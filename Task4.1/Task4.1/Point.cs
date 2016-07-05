f72676acc972743d9ba59b9f0d9c332ffd5eaa66
AnastasiyaKulakova <kylalova.n@mail.ru>
2016-07-05 16:35:31 +0300

    add

---
+++ Task4.1/Task4.1/Point.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4._1
+{
+    class Point
+    {
+       
+        public int x;
+        public int y;
+
+        public Point(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        static void Main(string[] args)
+        {
+          
+        }
+
+        
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Point p = (Point)obj;
+            return (x == p.x) && (y == p.y);
+        }
+
+        public override string ToString()
+        {
+            return "x= "+x+" y= "+y;
+        }
+
+    }
+}
